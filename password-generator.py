24be4ef9266afb32708a461a3be02340168a3254
Luke-PHS <144828720+Luke-PHS@users.noreply.github.com>
2025-02-12 18:24:17 +0000

    finished i think

--- password-generator.py
+++ password-generator.py
@@ -27,10 +27,25 @@ def fetch_word():
     response = requests.get(url)
     word = response.json()[0]
     return word
+def replaceLetters(word):
+    word = word[0].upper() + word[1:]
+    if "a" in word:
+        word = word.replace("a", "@")
+    if "t" in word:
+        word = word.replace("t", "+")
+    if "e" in word:
+        word = word.replace("e", "3")
+    if "i" in word:
+        word = word.replace("i", "!")    
+    return word
+    
+
 
 def generate_weaker_password():
     word1 = fetch_word()
     word2 = fetch_word()
+    word1 = replaceLetters(word1)
+    word2 = replaceLetters(word2)
     password = word1 + word2
     return password
 
